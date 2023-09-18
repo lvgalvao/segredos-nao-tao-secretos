dc5ddb392b6112cb465f7f31502bc59b8ad3f587
Luciano Filho <lvgalvaofilho@gmail.com>
2023-09-18 05:12:42 -0300

    codigo com segredos

---
+++ main.py
@@ -0,0 +1,13 @@
+#DADOS SENSÍVEIS
+EMAIL = 'lvgalvaofilho@gmail.com'
+PASSWORD = 'senha123'
+
+#MEUS SEGREDOS
+OPENAI_KEY = "openai-XYZABCDE"
+AWS_SECRET = "aws-s3cr3tk3y"
+
+#MEU APP
+print(f"Meu email: {EMAIL}")
+print(f"Minha senha: {PASSWORD}")
+print(f"Minha Key Open AI: {OPENAI_KEY}")
+print(f"Minha AWS Key: {AWS_SECRET}")
